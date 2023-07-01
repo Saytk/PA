8ff14c87a53b3c3548fd642ffd828d8d21f89354
ErwanBoumedine <erwan.boumedine@gmail.com>
2023-07-01 23:33:50 +0200

    MLP qui marche

--- ClionStuff/MultLayerPerceptron.cpp
+++ ClionStuff/MultLayerPerceptron.cpp
@@ -1,115 +1,101 @@
 #include "MultLayerPerceptron.h"
-#include <Eigen/Dense>
-#include <exception>
-#include <map>
-#include <functional>
-using namespace std;
 
-struct Layer
+Layer::Layer(int num_inputs, int num_outputs,
+             function<Eigen::VectorXd(Eigen::VectorXd)> activation,
+             function<Eigen::VectorXd(Eigen::VectorXd)> activation_derivative)
 {
-    Eigen::VectorXd weights;
-    Eigen::VectorXd bias;
-    function<Eigen::VectorXd(Eigen::VectorXd)> activation;
-    function<Eigen::VectorXd(Eigen::VectorXd)> activation_derivative;
+    this->weights = Eigen::MatrixXd::Random(num_outputs, num_inputs) / sqrt(num_inputs);
+    this->bias = Eigen::VectorXd::Random(num_outputs) / sqrt(num_inputs);
+    this->activation = activation;
+    this->activation_derivative = activation_derivative;
+}
 
-    Layer(int num_inputs, int num_outputs,
-          function<Eigen::VectorXd(Eigen::VectorXd)> activation,
-          function<Eigen::VectorXd(Eigen::VectorXd)> activation_derivative)
-    {
-        this->weights = Eigen::VectorXd::Random(num_inputs * num_outputs) / sqrt(num_inputs);
-        this->bias = Eigen::VectorXd::Random(num_outputs) / sqrt(num_inputs);
-        this->activation = activation;
-        this->activation_derivative = activation_derivative;
-    }
-};
+Eigen::MatrixXd Layer::getWeights() {
+    return weights;
+}
+Eigen::VectorXd Layer::getBias() {
+    return bias;
+}
+void Layer::setWeights(Eigen::MatrixXd newWeights) {
+    weights = newWeights;
+}
+void Layer::setBias(Eigen::VectorXd newBias) {
+    bias = newBias;
+}
+Eigen::VectorXd Layer::applyActivation(Eigen::VectorXd x) {
+    return activation(x);
+}
+Eigen::VectorXd Layer::applyActivationDerivative(Eigen::VectorXd x) {
+    return activation_derivative(x);
+}
 
-class MultLayerPerceptron
+MultLayerPerceptron::MultLayerPerceptron(int num_inputs, int num_outputs, int num_hidden_layers,
+                                         int num_neurons_per_hidden_layer, double learning_rate, string activation_func_name)
 {
-    vector<Layer> layers;
-    int num_inputs;
-    int num_outputs;
-    int num_hidden_layers;
-    int num_neurons_per_hidden_layer;
-    double learning_rate;
-    map<string, function<Eigen::VectorXd(Eigen::VectorXd)>> activation_functions;
-    map<string, function<Eigen::VectorXd(Eigen::VectorXd)>> activation_derivatives;
+    this->num_inputs = num_inputs;
+    this->num_outputs = num_outputs;
+    this->num_hidden_layers = num_hidden_layers;
+    this->num_neurons_per_hidden_layer = num_neurons_per_hidden_layer;
+    this->learning_rate = learning_rate;
 
-public:
-    MultLayerPerceptron(int num_inputs, int num_outputs, int num_hidden_layers,
-                        int num_neurons_per_hidden_layer, double learning_rate, string activation_func_name)
+    activation_functions["sigmoid"] = [](Eigen::VectorXd x) { return 1 / (1 + (-x.array()).exp()); };
+    activation_functions["tanh"] = [](Eigen::VectorXd x) { return x.array().tanh(); };
+    activation_functions["relu"] = [](Eigen::VectorXd x) { return x.cwiseMax(0); };
+
+    activation_derivatives["sigmoid"] = [](Eigen::VectorXd x) { return x.array() * (1 - x.array()); };
+    activation_derivatives["tanh"] = [](Eigen::VectorXd x) { return 1 - x.array().pow(2); };
+    activation_derivatives["relu"] = [](Eigen::VectorXd x) { return (x.array() > 0).cast<double>(); };
+
+    auto activation = activation_functions[activation_func_name];
+    auto activation_derivative = activation_derivatives[activation_func_name];
+
+    for (int i = 0; i < num_hidden_layers; i++)
     {
-        this->num_inputs = num_inputs;
-        this->num_outputs = num_outputs;
-        this->num_hidden_layers = num_hidden_layers;
-        this->num_neurons_per_hidden_layer = num_neurons_per_hidden_layer;
-        this->learning_rate = learning_rate;
-
-        activation_functions["sigmoid"] = [](Eigen::VectorXd x) { return 1 / (1 + (-x.array()).exp()); };
-        activation_functions["tanh"] = [](Eigen::VectorXd x) { return x.array().tanh(); };
-        activation_functions["relu"] = [](Eigen::VectorXd x) { return x.cwiseMax(0); };
-        // The softmax function should be applied to a vector of values, not to each value individually.
-        // Therefore, it does not fit well in this map. It will need to be handled separately.
-        // If you don't plan to use softmax in hidden layers, this should not be a problem.
-
-        activation_derivatives["sigmoid"] = [](Eigen::VectorXd x) { return x.array() * (1 - x.array()); };
-        activation_derivatives["tanh"] = [](Eigen::VectorXd x) { return 1 - x.array().pow(2); };
-        activation_derivatives["relu"] = [](Eigen::VectorXd x) { return (x.array() > 0).cast<double>(); };
-        auto activation = activation_functions[activation_func_name];
-        auto activation_derivative = activation_derivatives[activation_func_name];
-
-        for (int i = 0; i < num_hidden_layers; i++)
+        if (i == 0)
         {
-            if (i == 0)
-            {
-                layers.push_back(Layer(num_inputs, num_neurons_per_hidden_layer, activation, activation_derivative));
-            }
-            else
-            {
-                layers.push_back(Layer(num_neurons_per_hidden_layer, num_neurons_per_hidden_layer, activation, activation_derivative));
-            }
+            layers.push_back(Layer(num_inputs, num_neurons_per_hidden_layer, activation, activation_derivative));
         }
-
-        layers.push_back(Layer(num_neurons_per_hidden_layer, num_outputs, activation, activation_derivative));
+        else
+        {
+            layers.push_back(Layer(num_neurons_per_hidden_layer, num_neurons_per_hidden_layer, activation, activation_derivative));
+        }
     }
 
-    Eigen::VectorXd predict(Eigen::VectorXd input)
-    {
-        Eigen::VectorXd output = input;
-        for (int i = 0; i < layers.size(); i++)
-        {
-            output = layers[i].weights.transpose() * output;
-            output = layers[i].activation(output);
-        }
-        return output;
-    }
+    layers.push_back(Layer(num_neurons_per_hidden_layer, num_outputs, activation, activation_derivative));
+}
 
-    void train(Eigen::VectorXd input, Eigen::VectorXd target)
+Eigen::VectorXd MultLayerPerceptron::predict(Eigen::VectorXd input) {
+    Eigen::VectorXd output = input;
+    for (int i = 0; i < layers.size(); i++)
     {
-        Eigen::VectorXd output = input;
-        vector<Eigen::VectorXd> outputs;
+        output = layers[i].getWeights() * output + layers[i].getBias();
+        output = layers[i].applyActivation(output);
+    }
+    return output;
+}
+
+void MultLayerPerceptron::train(Eigen::VectorXd input, Eigen::VectorXd target) {
+    Eigen::VectorXd output = input;
+    vector<Eigen::VectorXd> outputs;
+    outputs.push_back(output);
+    for (int i = 0; i < layers.size(); i++)
+    {
+        output = layers[i].getWeights() * output + layers[i].getBias();
+        output = layers[i].applyActivation(output);
         outputs.push_back(output);
-        for (int i = 0; i < layers.size(); i++)
-        {
-            output = layers[i].weights.transpose() * output + layers[i].bias;
-            output = layers[i].activation(output);
-            outputs.push_back(output);
-        }
+    }
 
-        Eigen::VectorXd error = target - output;
-        for (int i = layers.size() - 1; i >= 0; i--)
-        {
-            error = layers[i].activation_derivative(outputs[i + 1]).cwiseProduct(error);
-            // Note: For a matrix A and vectors x, y, the outer product y*A*x is a matrix. 
-            // However, for a matrix A and vectors x, y, the expression y.transpose()*A*x is a scalar.
-            // So, to keep dimensions consistent, we need to use outer product here.
-            Eigen::MatrixXd delta = learning_rate * error * outputs[i].transpose();
-            layers[i].weights += delta;
-            layers[i].bias += learning_rate * error;
+    Eigen::VectorXd error = target - output;
+    for (int i = layers.size() - 1; i >= 0; i--)
+    {
+        error = layers[i].applyActivationDerivative(outputs[i + 1]).cwiseProduct(error);
+        Eigen::MatrixXd delta = learning_rate * error * outputs[i].transpose();
+        layers[i].setWeights(layers[i].getWeights() + delta);
+        layers[i].setBias(layers[i].getBias() + learning_rate * error);
 
-            if (i != 0)
-            {
-                error = layers[i].weights * error;
-            }
+        if (i != 0)
+        {
+            error = layers[i].getWeights().transpose() * error;
         }
     }
-};
+}
--- ClionStuff/MultLayerPerceptron.h
+++ ClionStuff/MultLayerPerceptron.h
@@ -1,11 +1,51 @@
-//
-// Created by erwan on 31/05/2023.
-//
-
 #ifndef PROJET_ANNUEL_MULTLAYERPERCEPTRON_H
 #define PROJET_ANNUEL_MULTLAYERPERCEPTRON_H
+
 #include <Eigen/Dense>
+#include <map>
+#include <functional>
+#include <vector>
 
+using namespace std;
 
+class Layer {
+public:
+    Layer(int num_inputs, int num_outputs,
+          function<Eigen::VectorXd(Eigen::VectorXd)> activation,
+          function<Eigen::VectorXd(Eigen::VectorXd)> activation_derivative);
+
+    Eigen::MatrixXd getWeights();
+    Eigen::VectorXd getBias();
+    void setWeights(Eigen::MatrixXd newWeights);
+    void setBias(Eigen::VectorXd newBias);
+
+    Eigen::VectorXd applyActivation(Eigen::VectorXd x);
+    Eigen::VectorXd applyActivationDerivative(Eigen::VectorXd x);
+
+private:
+    Eigen::MatrixXd weights;
+    Eigen::VectorXd bias;
+    function<Eigen::VectorXd(Eigen::VectorXd)> activation;
+    function<Eigen::VectorXd(Eigen::VectorXd)> activation_derivative;
+};
+
+class MultLayerPerceptron {
+public:
+    MultLayerPerceptron(int num_inputs, int num_outputs, int num_hidden_layers,
+                        int num_neurons_per_hidden_layer, double learning_rate, string activation_func_name);
+
+    Eigen::VectorXd predict(Eigen::VectorXd input);
+    void train(Eigen::VectorXd input, Eigen::VectorXd target);
+
+private:
+    vector<Layer> layers;
+    int num_inputs;
+    int num_outputs;
+    int num_hidden_layers;
+    int num_neurons_per_hidden_layer;
+    double learning_rate;
+    map<string, function<Eigen::VectorXd(Eigen::VectorXd)>> activation_functions;
+    map<string, function<Eigen::VectorXd(Eigen::VectorXd)>> activation_derivatives;
+};
 
 #endif //PROJET_ANNUEL_MULTLAYERPERCEPTRON_H
